7e957ad9dfd28519e0b8f1e6b9edb39d4a811168
Eric Farber <eric.j.farber@gmail.com>
2017-03-27 07:13:35 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+    if array.length < 2
+        array.join
+
+    elsif array.length < 3
+        array.join(" and ")
+    else
+        array[-1].prepend("and ")
+     array.join(", ")
+    end
 end
+
+# oxford_comma(["array", "asdfasdf", "dick"])
